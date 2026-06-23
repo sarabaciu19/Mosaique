b7af1f0d1a0107283b6d692e355cf1a0ef68fbda
Bianca <dancibianca1@gmail.com>
2026-06-23 19:48:20 +0300

    routes for login and register

--- mosaique-tracker/src/app/app.routes.ts
+++ mosaique-tracker/src/app/app.routes.ts
@@ -3,4 +3,17 @@ import { authGuard } from './core/guards/auth.guard';
 import { noAuthGuard } from './core/guards/no-auth.guard';
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: '**', redirectTo: 'login' }, // Wildcard route for a 404 page
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent),
+        canActivate: [noAuthGuard]
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent),
+        canActivate: [noAuthGuard]
+    }
+];
